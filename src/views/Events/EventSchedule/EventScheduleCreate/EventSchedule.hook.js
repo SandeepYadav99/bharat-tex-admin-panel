8af52169cc6f8b70b48d184391ef653391b2c955
sandeep.electrovese <sandeepelectrovese@gmail.com>
2024-02-06 19:28:13 +0530

    Event Schedule

--- src/views/Events/EventSchedule/EventScheduleCreate/EventSchedule.hook.js
+++ src/views/Events/EventSchedule/EventScheduleCreate/EventSchedule.hook.js
@@ -78,7 +78,6 @@ const useEventScheduleHook = ({
             end_time: data?.end_date_time,
             speakers: modifiedSpeaker,
             moderator: modifiedModerator,
-            category:data?.category,
             status: data?.status === Constants.GENERAL_STATUS.ACTIVE,
           });
         } else {
